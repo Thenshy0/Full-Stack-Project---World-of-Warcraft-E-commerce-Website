cefafbb77307976240a16d7e604738cbdd4cc57c
Thenshy0 <torok.mercedesz13@gmail.com>
2023-05-24 20:51:41 +0200

    home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,50 +10,61 @@ const Home = () => {
         <DesignCard></DesignCard>
       </div>
       <h2 className="main-h2">The best Tier-set awaits you</h2>
+
       <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+        style={
+          {
+            // display: "flex",
+            // justifyContent: "center",
+            // alignItems: "center",
+          }
+        }
       >
-        <Card
-          sx={{
-            maxWidth: 700,
-            margin: "2rem",
-            backgroundColor: "rgb(182, 180, 184)",
-          }}
-        >
-          <CardContent sx={{ margin: "1rem" }}>
-            <Typography>
-              Experience the thrill of browsing through our virtual armory,
-              where every set tells a story and exudes the essence of Azeroth.
-              Whether you seek the elegance of elven robes or the rugged
-              resilience of plate mail, our diverse range of sets caters to all
-              adventurers, ensuring that you look and feel your best on every
-              quest.
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card
-          sx={{
-            maxWidth: 700,
-            margin: "2rem",
-
-            backgroundColor: "rgb(182, 180, 184)",
-          }}
-        >
-          <CardContent sx={{ margin: "1rem" }}>
-            <Typography>
-              Shop with confidence as we prioritize quality and authenticity,
-              sourcing our sets from reputable craftsmen and reputable sources
-              within the World of Warcraft community. Join countless adventurers
-              who have entrusted us with their armor needs and embark on your
-              journey to become a true legend.
-            </Typography>
-          </CardContent>
-        </Card>
+        <div className="home-cover-pic">
+          <div className="home-cover-pic-card">
+            <Card
+              className="cover-card"
+              sx={{
+                maxWidth: 500,
+                margin: "1rem",
+                border: "solid 2px #ab8f5c",
+                borderRadius: "0.5rem",
+              }}
+            >
+              <CardContent sx={{ margin: "0.5rem", color: "#ffd384" }}>
+                <Typography>
+                  Join countless adventurers who have entrusted us with their
+                  armor needs and embark on your journey to become a true
+                  legend.
+                </Typography>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="home-cover-pic-card-two">
+            {" "}
+            <Card
+              className="cover-card"
+              sx={{
+                maxWidth: 700,
+                margin: "2rem",
+                border: "solid 2px #ab8f5c",
+                borderRadius: "0.5rem",
+              }}
+            >
+              <CardContent sx={{ margin: "0.5rem", color: "#ffd384" }}>
+                <Typography>
+                  Experience the thrill of browsing through our virtual armory,
+                  where every set tells a story and exudes the essence of
+                  Azeroth. Whether you seek the elegance of elven robes or the
+                  rugged resilience of plate mail, our diverse range of sets
+                  caters to all adventurers, ensuring that you look and feel
+                  your best on every quest.
+                </Typography>
+              </CardContent>
+            </Card>{" "}
+          </div>
+        </div>
+        <div> </div>
       </div>
       <div
         style={{
