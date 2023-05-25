9cf7361ce09b2fb4638fe3325d7daa6a5b912a4c
Thenshy0 <torok.mercedesz13@gmail.com>
2023-05-25 13:14:24 +0200

    n/a

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,7 @@ const apilimiter = rateLimit({
 
 app.use(
   cors({
-    original: ["http://127.0.0.1:3000", "http://localhost:3000"],
+    original: [process.env.CLIENT_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
